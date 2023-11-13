ac1e3623edf97168f37529a5556834497b31cd01
Gil Vernik <gvernik@gmail.com>
2023-11-13 10:12:48 -0800

    fixes for the paper

--- skystore_cli.py
+++ skystore_cli.py
@@ -122,7 +122,7 @@ def register(
         server_addr = "localhost"
     else:
         # NOTE: ip address set to be the remote store-server addr
-        server_addr = "54.183.123.82"
+        server_addr = "18.144.171.244"
 
     try:
         with open(register_config, "r") as f:
