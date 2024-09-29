b24a64921aa4c445cd7e8520e137bb66fe4b9a08
Erez Hadad <erezh@il.ibm.com>
2024-09-29 22:59:26 +0300

    Added config suffix handling for mk_s3proxy_cm.sh

--- k8s/build/mk_s3proxy_cm.sh
+++ k8s/build/mk_s3proxy_cm.sh
@@ -4,5 +4,10 @@ if ! [ -f "$1" ]; then
   echo "Invalid env file"
 fi
 
+suffix="$2"
 
-kubectl create configmap conf-skystore-s3proxy --from-env-file=$1
+# Prepend dash if suffix is non-empty and does not start with dash
+[[ -n "$suffix" ]] && ! [[ "$suffix" == -* ]] && suffix="-$suffix"
+
+
+kubectl create configmap conf-skystore-s3proxy"$suffix" --from-env-file=$1
