75f3cfa832c0327804e0fce25bce0e586fb47724
Erez Hadad <erezh@il.ibm.com>
2025-01-13 13:51:55 +0200

    Added support for a second argument for tag suffix in scripts that handle images

--- docker/build/tag_all.sh
+++ docker/build/tag_all.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 prefix=$1
+TAG_SUFFIX=$2
+
+: ${TAG_SUFFIX:=latest}
 
 # Add slash if prefix is non-empty and does not end with slash
 [[ -n "$prefix" ]] && ! [[ "$prefix" == */ ]] && prefix="$prefix/"
 
 for n in base server s3proxy; do
-    docker tag skystore-$n:latest "$prefix"skystore-$n:latest;
+    docker tag skystore-$n:latest "$prefix"skystore-$n:$TAG_SUFFIX;
 done
  
--- docker/build/untag_all.sh
+++ docker/build/untag_all.sh
@@ -2,10 +2,13 @@
 
 prefix=$1
 
+TAG_SUFFIX=$2
+: ${TAG_SUFFIX:=latest}
+
 # Add slash if prefix is non-empty and does not end with slash
 [[ -n "$prefix" ]] && ! [[ "$prefix" == */ ]] && prefix="$prefix/"
 
 for n in base server s3proxy; do
-    docker image rm -f "$prefix"skystore-$n:latest;
+    docker image rm -f "$prefix"skystore-$n:$TAG_SUFFIX;
 done
  
