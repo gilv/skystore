85ad2b5ed5a79d63df4ceb764b8660125f598886
Erez Hadad <erezh@il.ibm.com>
2026-02-01 20:52:10 +0200

    Added more commented options to build_all.sh

--- docker/build/build_all.sh
+++ docker/build/build_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Uncomment below to force clean build
-# export SKY_BUILD_ARGS="--no-cache"
+# Uncomment below to force clean build and plain output for logs
+# export SKY_BUILD_ARGS="--no-cache --progress=plain"
 
 for n in base server s3proxy; do
     cd $n;
