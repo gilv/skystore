e31de48beaf2516c364c7e46c1b23de36ce435b8
Erez Hadad <erezh@il.ibm.com>
2026-02-22 22:38:09 +0200

    add_*_all.sh scripts now work wherever called

--- docker/run/add_s3proxy_all.sh
+++ docker/run/add_s3proxy_all.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-../add_s3proxy_env_prv_key.sh env.base
-../add_s3proxy_env_s3_cfg.sh env.base.withprvkey aws.config
-../add_s3proxy_env_s3pjson_cfg.sh env.base.withprvkey.withs3cfg s3proxy.json
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Call scripts from the script's directory
+"$SCRIPT_DIR/add_s3proxy_env_prv_key.sh" env.base
+"$SCRIPT_DIR/add_s3proxy_env_s3_cfg.sh" env.base.withprvkey aws.config
+"$SCRIPT_DIR/add_s3proxy_env_s3pjson_cfg.sh" env.base.withprvkey.withs3cfg s3proxy.json
 mv env.base.withprvkey.withs3cfg.withs3pjsoncfg env.final
 rm -f *with*
 
--- docker/run/add_server_all.sh
+++ docker/run/add_server_all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-../add_server_env_pub_key.sh env.base
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Call add_server_env_pub_key.sh from the script's directory
+"$SCRIPT_DIR/add_server_env_pub_key.sh" env.base
 mv env.base.withpubkey env.final
 rm -f *with*
 
